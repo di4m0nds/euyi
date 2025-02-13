5863f2312a9e0c0bb9678511d2fe7b2a272ad3ed
slvt <ws.2000.sja@gmail.com>
2025-02-13 23:37:34 +0100

    Fix Navbar items

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ function Navbar() {
     const linkStyle = "navbar-link hover:text-orange-200 font-normal hover:font-medium hover:underline ";
 
     return (
-        <nav className="w-full md:w-[45%] mt-8 md:mt-0">
+        <nav className="w-full h-full flex items-center justify-center md:w-[45%] mt-8 md:mt-0">
             <ul className="flex w-full md:justify-evenly justify-around">
                 <li className="navbar-item">
                     <a href="#trabajos" className={linkStyle}>
