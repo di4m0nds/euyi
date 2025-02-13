80138c69c8c5b82af40c3c1cc05564f6cdc276a1
slvt <ws.2000.sja@gmail.com>
2025-02-13 23:39:10 +0100

    Making navbar stickish

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,9 @@ import { Header, About, Projects, Experience, Testimonials, Footer } from "../..
 function Home() {
   return(
     <div className="w-screen">
-        <Header activeNavbar={true} />
+        <div className="sticky top-0 z-50 pb-20 lg:pb-6 bg-gradient-to-t from-transparent via-black/90 to-black">
+          <Header activeNavbar={true} />
+        </div>
         <main className="w-[90%] xl:w-[70%] m-auto">
             <About />
             <Projects />
